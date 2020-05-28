453ca2ec9dc8b44a1cbd235c53922bfab7a2e22b
ismailkasan <ismailkasan@piton.com.tr>
2020-05-28 22:43:15 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            var config = new ProducerConfig () { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new Producer<string, string> (config)) {
+                while (true) {
+                    Console.Write ("Enter message: ");
+                    var text = Console.ReadLine ();
+
+                    Message<string, string> message = new Message<string, string> { Value = text }; 
+                    DeliveryReport<string, string> result = producer.ProduceAsync ("ToDoList", message).GetAwaiter().GetResult();    
+                    Console.WriteLine ($"Delivered to '{result.TopicPartitionOffset}'");
+                }
+            }
+        }
+    }
+}
